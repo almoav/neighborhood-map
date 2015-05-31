2c87f3abe755042af97e4fbb5b369b8cfb341309
Alex <almoav@gmail.com>
2015-05-30 21:34:35 -0700

    info window loads place details

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var viewMap = {
 	    	center: new google.maps.LatLng(-34.397, 150.644)
 	  	};
 
+	  	this.marker = {};
 		this.map = new google.maps.Map(document.getElementById('mapView'),
 	    	mapOptions);
 
@@ -29,10 +30,56 @@ var viewMap = {
 		}
 	},
 
+	placeDetails: function(place_id) {
+		//console.log(marker, place_id);
+		var request = {
+		  placeId: place_id
+		};
+		var service = new google.maps.places.PlacesService(this.map);
+		service.getDetails(request, this.detailsCallback);
+	},
+
 	textCallback: function(results, status) {
 		if (status == google.maps.places.PlacesServiceStatus.OK) {
 	    	viewMap.createMarker(results[0]);
+			//console.log(results[0]);
+			//viewMap.placeDetails(results[0].place_id);
 	    }
+	},
+
+	detailsCallback: function(results, status) {
+		console.log('details callback', status);
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			//console.log(results);
+			viewMap.setInfowindow(results);
+			//viewMap.infowindow.open(this.map, this.marker);
+		}
+	},
+
+	setInfowindow: function(placeData) {
+    	//console.log(placeData);
+    	var url = '';
+    	if (placeData.photos) {
+    		url = placeData.photos[0].getUrl({'maxWidth': 300, 'maxHeight': 200});
+    	}
+
+    	var money = ' ';
+    	for (i = 0; i < placeData.price_level; i++) {
+    		money += "$";
+    	}
+
+    	viewMap.infowindow.setContent(
+    		'<div>' +
+    		'<h2>' + placeData.name + '</h2>' +
+    		'<img alt="" src="'+url+'">' +
+    		'<p>' + placeData.formatted_address + '</p>' +
+    		'<strong>' + placeData.rating + money + '</strong>' +
+    		'<p>' + placeData.formatted_phone_number + '</p>' +
+    		'</div>'
+    	);
+
+    	viewMap.infowindow.open(this.map, this.marker);
+
 	},
 
 	createMarker: function(placeData) {
@@ -42,12 +89,14 @@ var viewMap = {
 	    var image = {
 	    	url: placeData.icon,
 	    	size: new google.maps.Size(48, 48),
-	    	//origin: new google.maps.Point(12,12),
-	    	//anchor: new google.maps.Point(0, 12),
 	    	scaledSize: new google.maps.Size(24, 24),
 	    };
+
 	    var name = placeData.name;
 	    //var name = placeData.formatted_address;
+	    var address = placeData.adr_address;
+
+
 		var marker = new google.maps.Marker({
 		    map: this.map,
 		    position: placeData.geometry.location,
@@ -57,19 +106,10 @@ var viewMap = {
 
 	    // marker click listener
 	    google.maps.event.addListener(marker, 'click', function() {
-	    	var url = '';
-	    	if (placeData.photos) {
-	    		url = placeData.photos[0].getUrl({'maxWidth': 300, 'maxHeight': 200});
-	    	}
-
-
-	    	viewMap.infowindow.setContent(
-	    		'<h2>' + name + '</h2>' +
-	    		'<img alt="" src="'+url+'">' +
-	    		'<p>' + placeData.formatted_address + '</p>'
-	    		);
-	    	viewMap.infowindow.open(this.map, marker);
+	    	viewMap.marker = marker;
+	    	viewMap.placeDetails(placeData.place_id);
 	    });
+
 
 	    this.bounds.extend(new google.maps.LatLng(lat, lon));
 	    // fit the map to the new marker
@@ -78,10 +118,7 @@ var viewMap = {
 	    this.map.setCenter(this.bounds.getCenter());
 		},
 
-	placeDetails: function(place_id) {
-		var service = new google.maps.places.PlacesService(this.map);
-		service.getDetails(request, callback);
-	}
+
 
 };
 window.onLoad = viewMap.init();
