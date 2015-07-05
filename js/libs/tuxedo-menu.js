7ec383043fb1e68526c531d248a70bd5ac64f04f
Alex <almoav@gmail.com>
2015-07-04 19:40:29 -0700

    add basic slide menu functionality

---
+++ js/libs/tuxedo-menu.js
@@ -0,0 +1,47 @@
+(function ($) {
+	'use strict';
+	var settings;
+
+	$.fn.tuxedoMenu = function (options) {
+		var self = this;
+
+		// Extend default settings with options
+		settings = $.extend({
+			triggerSelector: '#tuxedo-menu-trigger',
+			menuSelector: '.tuxedo-menu',
+			isFixed: true
+		}, options);
+
+		self.addClass('tuxedo-menu tuxedo-menu-pristine animated')
+			.toggleClass('tuxedo-menu-visible', !settings.isFixed)
+			.toggleClass('tuxedo-menu-fixed slideOutLeft', settings.isFixed);
+		$(settings.triggerSelector).addClass('tuxedo-menu-trigger');
+
+		$(settings.triggerSelector).click(function () {
+			if (!settings.isFixed) {
+				return;
+			}
+
+			$(settings.menuSelector)
+				.toggleClass('slideInLeft slideOutLeft')
+				.addClass('tuxedo-menu-visible');
+		});
+
+		$(document).click(function (event) {
+			if (!settings.isFixed ||
+				$(event.target).is(settings.triggerSelector)) {
+				return;
+			}
+
+			if (!$(event.target).is(settings.triggerSelector) &&
+				!$(event.target).closest(settings.menuSelector).length) {
+
+				$(settings.menuSelector)
+					.removeClass('slideInLeft')
+					.addClass('slideOutLeft');
+			}
+		});
+
+		return self;
+	};
+}(jQuery));
